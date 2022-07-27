2c38a6f1bf821455f57f028b3b8c76c10d57a151
Mati Czarnecki <maticzacza@gmail.com>
2022-07-27 21:35:13 +0200

    user stories added

--- Project/dist/index.62406edb.js
+++ Project/dist/index.62406edb.js
@@ -5,7 +5,20 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showArticle = async function() {
+    try {
+        const response = await fetch("https://newsapi.org/v2/everything?q=Samsung&language=en&from=2022-07-27&sortBy=popularity&apiKey=dc297ae8299e47b7b6f153d8f0dd2d73");
+        const data = await response.json();
+        const articles = data.articles;
+        console.log(articles);
+        if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    } catch (err) {
+        alert(err);
+    }
+};
+showArticle();
 
 //# sourceMappingURL=index.62406edb.js.map
--- Project/src/js/controller.js
+++ Project/src/js/controller.js
@@ -11,3 +11,20 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showArticle = async function () {
+  try {
+    const response = await fetch(
+      'https://newsapi.org/v2/everything?q=Samsung&language=en&from=2022-07-27&sortBy=popularity&apiKey=dc297ae8299e47b7b6f153d8f0dd2d73'
+    );
+    const data = await response.json();
+    const articles = data.articles;
+    console.log(articles);
+    if (!response.ok) {
+      throw new Error(`${data.message} (${response.status})`);
+    }
+  } catch (err) {
+    alert(err);
+  }
+};
+showArticle();
